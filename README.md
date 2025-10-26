e552cf2aca472ca1be8aca9b62a32cc97b33d45b
Seth-David-Kay <kaysdk@gmail.com>
2025-10-26 13:17:34 -0400

    new flow

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ OCR and automatic linking
 
 1. Organize your folder with the PDFs you want to process in a folder named `pdfs`. Other folders will be ignored.
 2. Keep the `pdfs` folder in the folder hierarchy; everything else does not matter.
+22. Source .envrc
 3. Run the script. It will place all the `.md` files in the `automation` folder on your desktop.
 4. The script will also create a `general.md` file in the folder where the script is located. You can then move your PDFs, `.md` files, and `general.md` to Obsidian.
 5. Ensure that Ollama is open and running on your local machine.
@@ -12,3 +13,6 @@ OCR and automatic linking
 7. Verify that `.DS_Store` has been removed from the `pdfs` folder before running the script. -> should be fixed now
 
 # Personal Note: use python3 command not python when running
+
+# Notes
+- workflow (basic): put pdfs into folders corresponding to what classes they are, output is output folders for each class that include what to add to the overview, what notes to paste into the overarching folder, and what pdfs to paste into the pdf section. rename pdfs myself when pasting them in, and take the file names as input (can be a cli input if I want to try renaming them, but this is mvp)
--- automateObdianNotes.py
+++ automateObdianNotes.py
@@ -1,14 +1,14 @@
 import PIL.Image
 import google.generativeai as genai
 from pdf2image import convert_from_path
+from pathlib import Path
 import requests
 import json
 import os
-
-path = os.environ['LOCAL_PATH_FOLDER']
-save_path = os.environ['SAVE_PATH']
+import sys
 
 def generate_full_reponse_gemma(prompt):
+    return "hi"
     r = requests.post('http://0.0.0.0:11434/api/generate',
                       json={
                           'model': "gemma:2b",
@@ -28,9 +28,10 @@ def generate_full_reponse_gemma(prompt):
     return full_response
 
 def get_text_from_image(prompt, images):
+    return "hi"
     GOOGLE_API_KEY=os.environ['GOOGLE_API_KEY']
     genai.configure(api_key=GOOGLE_API_KEY)
-    model = genai.GenerativeModel('gemini-1.5-pro-002')
+    model = genai.GenerativeModel('gemini-2.5-pro')
 
     # Give it the prompt + the list containing all properly formatted images
     response = model.generate_content([prompt] + images, stream=False)
@@ -42,8 +43,8 @@ def get_text_from_image(prompt, images):
     except:
         return ""
 
-def convert_pdf_to_image(pdf_file):
-    images = convert_from_path(f"{path}/pdfs/" + pdf_file)
+def convert_pdf_to_image(folder_path, pdf_file):
+    images = convert_from_path(f"{folder_path}/" + pdf_file)
 
     # Array to store all images properly formatted
     images_array = []
@@ -57,36 +58,43 @@ def convert_pdf_to_image(pdf_file):
 
     return images_array
 
-def read_summarize_pdf(notes_pdf, prompt, filename_prompt):
+def read_summarize_pdf(folder_path, notes_pdf, class_name, prompt, filename_prompt, change_filename):
     # Convert notes pdf to png here
-    notes_images = convert_pdf_to_image(notes_pdf)
+    notes_images = convert_pdf_to_image(folder_path, notes_pdf)
 
     # Get summary response from gemini
     response = get_text_from_image(prompt, notes_images)
     # Add the link to the pdf file to the input
     response = response + f"\n\n[[{notes_pdf}]]"
 
-    # Get the filename from local gemma
-    output_file_name = generate_full_reponse_gemma(f"{filename_prompt} Summary: {response}")
-    # Remove everything after the first '.', a.k.a the file type
-    output_file_name = output_file_name.split('.', 1)[0]
-    # If the response has a new line character or is too long just default the file name to 'generic'
-    # Also if the filename is just a class name, default the file name to 'generic'
-    if (len(output_file_name) > 20):
-        output_file_name = notes_pdf + " notes"
+    if change_filename:
+        # Get the filename from local gemma
+        output_file_name = generate_full_reponse_gemma(f"{filename_prompt} Summary: {response}")
+        # Remove everything after the first '.', a.k.a the file type
+        output_file_name = output_file_name.split('.', 1)[0]
+        # If the response has a new line character or is too long just default the file name to 'generic'
+        # Also if the filename is just a class name, default the file name to 'generic'
+        if (len(output_file_name) > 1):
+            output_file_name = notes_pdf[:-4]
+    else:
+        # Just remove the .pdf stupidly, by truncating the pdf filename by 4 characters
+        output_file_name = notes_pdf[:-4]
 
-    f = open(f"{output_file_name}.md", "w")
+    f = open(f"{folder_path}/{output_file_name}.md", "w")
 
     f.write(response)
     f.close()
 
-    # Move the file to a new directory. If it already exists as a filename, keep adding to it until it doesn't.
-    old_filepath = f"{path}/{output_file_name}.md"
-    new_filepath = f"{save_path}/{output_file_name}.md"
     # If there is not Automated folder on the Desktop, make one
     if not os.path.exists(f"{save_path}"):
         os.mkdir(f"{save_path}")
+    if not os.path.exists(f"{save_path}/{class_folder}"):
+        os.mkdir(f"{save_path}/{class_folder}")
+        os.mkdir(f"{save_path}/{class_folder}/pdfs")
+
     # Move .md file to a new directory in order not to be overwritten
+    old_filepath = f"{folder_path}/{output_file_name}.md"
+    new_filepath = f"{save_path}/{class_folder}/{output_file_name}.md"
     i = 1
     while True:
         # If the file doesn't exist
@@ -94,18 +102,31 @@ def read_summarize_pdf(notes_pdf, prompt, filename_prompt):
             os.rename(old_filepath, new_filepath)
             break
         # If it does exist, add 1 to the filename and try again
-        new_filepath = f"{save_path}/{output_file_name}{i}.md"
+        new_filepath = f"{save_path}/{class_folder}/{output_file_name}-{i}.md"
         i += 1
 
     # Append this filename to the overview file
     if i == 1:
-        fo = open(f"Overview.md", "a")
+        fo = open(f"{save_path}/{class_folder}/Overview.md", "a")
         fo.write(f"[[{output_file_name}]]\n")
         fo.close()
     else:
-        fo = open(f"Overview.md", "a")
-        fo.write(f"[[{output_file_name}{i - 1}]]\n")
+        fo = open(f"{save_path}/{class_folder}/Overview.md", "a")
+        fo.write(f"[[{output_file_name}-{i - 1}]]\n")
         fo.close()
+
+    # Move .pdf file to new directory and not have it be overwritten
+    old_filepath = f"{folder_path}/{notes_pdf}"
+    new_filepath = f"{save_path}/{class_folder}/pdfs/{notes_pdf}"
+    i = 1
+    while True:
+        # If the file doesn't exist
+        if not os.path.isfile(new_filepath):
+            os.rename(old_filepath, new_filepath)
+            break
+        # If it does exist, add 1 to the filename and try again
+        new_filepath = f"{save_path}/{class_folder}/pdfs/{i}-{notes_pdf}"
+        i += 1
 
     # Now remove all of the temporary png files we created:
     png_pathname = f"{path}/page0.png" # The first one will always be called this, and we will always have at least one
@@ -118,7 +139,6 @@ def read_summarize_pdf(notes_pdf, prompt, filename_prompt):
         os.remove(png_pathname)
         png_pathname = f"{path}/page{i}.png"
         i += 1
-
 
 # Create a long summary prompt and a short summary prompt (long summary is what it usually does short summary is a few bullet points of only overarching topics)
 
@@ -136,16 +156,39 @@ short_summary_prompt = "Create an obsidian file to summarize briefly the main po
 filename_prompt = "Given the following summary of class notes file, give a meaningful filename to store the summary in.\
     Write nothing else. Use underscores in between words." # Hard coded the class names to not use in here
 
-
 # Run through all pdfs in 'pdfs' folder and run read_summarize_pdf on all of their pathnames
-pdf_files = []
-for (filenames) in os.walk(f"{path}/pdfs"):
-    pdf_files.extend(filenames)
-    break
+cli_input = sys.argv
+if len(cli_input) < 2:
+    print("Missing one input: <env folder>. Optional secondary argument: <save path>.")
+    print("<env folder> must be a folder that contains an env.md file with " \
+    "the course names seperated by commas (no spaces), and it must contain folders with those" \
+    "course names with the pdf files to process inside the corresponding folder. Pdf files will" \
+    "not be renamed. Folder names must match exactly.")
+    sys.exit()
 
-pdf_files = pdf_files[2] # At 2 because the first is the folder we are in, second is script file, so the third element is the one we want
+path = sys.argv[1]
+if len(sys.argv) > 2:
+    save_path = sys.argv[2]
+else:
+    save_path = os.environ["SAVE_PATH"]
 
-for file in pdf_files:
-    if file == ".DS_Store":
+env = open(f"{path}/env.md")
+classes = env.read().split(",")
+class_folders = []
+for class_name in classes:
+    class_folders.append(class_name.strip())
+if input(f"{class_folders}: press enter to continue.") != "":
+    sys.exit()
+
+for class_folder in class_folders:
+    folder_path = Path(path, class_folder)
+    if not folder_path.exists():
+        print(f"Folder not found: {folder_path}")
         continue
-    read_summarize_pdf(file, short_summary_prompt, filename_prompt)
+    pdf_files = [p.name for p in folder_path.iterdir() if p.is_file() and p.suffix.lower() == ".pdf"]
+    print(f"{class_folder} : {pdf_files}")
+    for file in pdf_files:
+        if file == ".DS_Store":
+            continue
+        folder_path = f"{path}/{class_folder}"
+        read_summarize_pdf(folder_path, file, class_folder, short_summary_prompt, filename_prompt, False)
